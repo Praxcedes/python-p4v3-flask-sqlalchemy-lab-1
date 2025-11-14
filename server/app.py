d1027bdbbc4043a6fa87f337dc3eb53e57009ec4
Praxcedes <praxcedeskabeya@gmail.com>
2025-11-14 10:56:29 +0300

    python-p4v3-flask-sqlalchemy-lab-1/server

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -14,14 +13,52 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return make_response(jsonify(body), 200)
 
-# Add views here
+# ---------------------------
+# Task 3: Get earthquake by ID
+# ---------------------------
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
 
+    if quake:
+        body = {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        return make_response(jsonify(body), 200)
+
+    return make_response(
+        jsonify({"message": f"Earthquake {id} not found."}),
+        404
+    )
+
+# -----------------------------------------------
+# Task 4: Get earthquakes with minimum magnitude
+# -----------------------------------------------
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    body = {
+        "count": len(quakes),
+        "quakes": [
+            {
+                "id": q.id,
+                "location": q.location,
+                "magnitude": q.magnitude,
+                "year": q.year
+            } for q in quakes
+        ]
+    }
+
+    return make_response(jsonify(body), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
